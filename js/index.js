c6578f80255a27e606ec8b307aa73996b57e55a7
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2018-10-12 12:37:46 +0900

    Remove jQuery, jQuery UI and FontAwesome

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-var state = [];
-
 function setDefaultState() {
   var id = generateID();
   var baseState = {};
@@ -56,125 +54,45 @@ function getState() {
 
 function addItem(text, status, id, noUpdate) {
   var id = id ? id : generateID();
-  var c = status === 'done' ? 'danger' : '';
+  var klass = status === 'done' ? 'danger' : '';
   var item =
-    '<li data-id="' + id + '" class="animated flipInX ' + c + '">' +
-    '<div class="checkbox"><span class="close"><i class="fa fa-times"></i>' +
-    '</span><label><span class="checkbox-mask"></span>' +
-    '<input type="checkbox" />' +
-    text +
-    '</label></div></li>';
+    '<li data-id="' + id + '" class="' + klass + '">' +
+    '<div class="checkbox">' +
+      '<span class="close">x</span>' +
+      '<label><span class="checkbox-mask"></span>' +
+      '<input type="checkbox" />' + text + '</label>' +
+    '</div>' +
+    '</li>';
 
-  var isError = $('.form-control').hasClass('hidden');
+  var control = document.querySelector('.form-control');
+  var err = document.querySelector('.err');
 
-  var err = document.getElementsByClassName('err')[0];
   if (text === '') {
     err.classList.remove('hidden');
   } else {
     err.classList.add('hidden');
-    $('.todo-list').append(item);
+    document.querySelector('.todo-list').insertAdjacentHTML(
+      'afterbegin', item);
   }
 
-  $('.refresh').removeClass('hidden');
-  $('.no-items').addClass('hidden');
-  $('.form-control')
-    .val('')
-    .attr('placeholder', '✍️ Add item...');
+  control.value = '';
+  control.setAttribute('placeholder', 'Add item...');
   setTimeout(function() {
-    $('.todo-list li').removeClass('animated flipInX');
-  }, 500);
+    let elem = document.querySelector('.todo-list li');
+    if (elem != null) {
+      elem.classList.remove('animated');
+    }
+  }, 150);
 
   if (!noUpdate) {
     pushToState(text, 'new', id);
   }
 }
 
-function refresh() {
-  $('.todo-list li').each(function(i) {
-    $(this)
-      .delay(70 * i)
-      .queue(function() {
-        $(this).addClass('animated bounceOutLeft');
-        $(this).dequeue();
-      });
-  });
-
-  setTimeout(function() {
-    $('.todo-list li').remove();
-    $('.no-items').removeClass('hidden');
-    $('.err').addClass('hidden');
-  }, 800);
-}
-
-$(function() {
-  var err = $('.err'),
-    formControl = $('.form-control'),
-    isError = formControl.hasClass('hidden');
-
-  if (!isError) {
-    formControl.blur(function() {
-      err.addClass('hidden');
-    });
-  }
-
-  $('.add-btn').on('click', function() {
-    var itemVal = $('.form-control').val();
-    addItem(itemVal);
-    formControl.focus();
-  });
-
-  $('.refresh').on('click', refresh);
-
-  $('.todo-list').on('click', 'input[type="checkbox"]', function() {
-    var li = $(this)
-      .parent()
-      .parent()
-      .parent();
-    li.toggleClass('danger');
-    li.toggleClass('animated flipInX');
-
-    setToDone(li.data().id);
-
-    setTimeout(function() {
-      li.removeClass('animated flipInX');
-    }, 500);
-  });
-
-  $('.todo-list').on('click', '.close', function() {
-    var box = $(this)
-      .parent()
-      .parent();
-
-    if ($('.todo-list li').length == 1) {
-      box.removeClass('animated flipInX').addClass(
-        'animated bounceOutLeft');
-      setTimeout(function() {
-        box.remove();
-        $('.no-items').removeClass('hidden');
-        $('.refresh').addClass('hidden');
-      }, 500);
-    } else {
-      setTimeout(function() {
-        box.remove();
-      }, 500);
-    }
-    deleteTodo(box.data().id)
-  });
-
-  $('.form-control').keypress(function(e) {
-    if (e.which == 13) {
-      var itemVal = $('.form-control').val();
-      addItem(itemVal);
-    }
-  });
-  $('.todo-list').sortable();
-  $('.todo-list').disableSelection();
-});
-
 var dateLine = document.querySelector('.today');
 dateLine.innerHTML = new Date();
 
-$(document).ready(function() {
+document.addEventListener('DOMContentLoaded', function(event) {
   var state = getState();
 
   if (!state) {
@@ -186,4 +104,62 @@ $(document).ready(function() {
     var todo = state[todoKey];
     addItem(todo.title, todo.status, todo.id, true);
   });
+
+  var control = document.querySelector('.form-control')
+    , isError = control.classList.contains('hidden')
+    ;
+
+  if (!isError) {
+    var err = document.querySelector('.err');
+    err.classList.add('hidden');
+  }
+
+  var btn = document.querySelector('.add-btn');
+  btn.addEventListener('click', function(event) {
+    addItem(control.value);
+    control.focus();
+  });
+
+  var nodeList;
+
+  // check
+  nodeList = document.querySelectorAll('.todo-list input[type="checkbox"]');
+  Array.prototype.slice.call(nodeList).map(function(element) {
+    element.addEventListener('click', function(event) {
+      var li = this.parentNode.parentNode.parentNode;
+      li.classList.toggle('danger');
+      li.classList.toggle('animated');
+
+      setToDone(li.getAttribute('data-id'));
+      setTimeout(function() {
+        li.classList.remove('animated');
+      }, 150);
+    });
+  });
+
+  // done
+  nodeList = document.querySelectorAll('.todo-list .close');
+  Array.prototype.slice.call(nodeList).map(function(element) {
+    element.addEventListener('click', function(event) {
+      var box = this.parentNode.parentNode;
+
+      var tasksCount = document.querySelectorAll('.todo-list li').length;
+      setTimeout(function() {
+        box.remove();
+        if (tasksCount === 1) {
+          document.querySelector('.no-items').classList.remove('hidden');
+        }
+      }, 200);
+      deleteTodo(box.getAttribute('data-id'));
+    });
+  });
+
+  control.addEventListener('keypress', function(event) {
+    if (event.key.toString() === 'Enter') {
+      addItem(this.value);
+    }
+  });
+
+  var list = document.querySelector('.todo-list');
+  Sortable.create(list);
 });
